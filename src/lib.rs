4b87a063151050d6e4340ce5e0d99fc39747e77d
Evgenii Nekhoroshev <uqenekho@eait.uq.edu.au>
2026-07-10 14:11:14 +1000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// lib.rs
+
+//! Triangually packed matrix representations for efficient use with LAPACK algorithms.
+
+pub struct PackedLower {}
+
+pub struct PackedUpper {}
+
+pub struct PackedSymmetric {}
+
+pub struct PackedSPD {}
+
+pub struct PackedHermitian {}
+
