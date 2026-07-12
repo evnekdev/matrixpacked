2a35185726ecddcf9000e5b4e3f1206f019359f5
Evgenii Nekhoroshev <evgnekhoroshev@gmail.com>
2026-07-12 17:43:45 +1000

    Fix Complex32 literals in CHPTRS example

--- examples/lapack_hermitian_c32_hptrs.rs
+++ examples/lapack_hermitian_c32_hptrs.rs
@@ -8,11 +8,11 @@ use num_complex::Complex32;
 use matrixpacked::PackedHermitianView;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let storage = [Complex32::new(2, 0), Complex32::new(1, 1), Complex32::new(-1, 0)];
+    let storage = [Complex32::new(2.0, 0.0), Complex32::new(1.0, 1.0), Complex32::new(-1.0, 0.0)];
     let a = PackedHermitianView::<Complex32>::from_slice(2, &storage)?;
     let factor = a.factorize()?;
-    let mut b = [Complex32::new(4, -2), Complex32::new(-1, 1)];
+    let mut b = [Complex32::new(4.0, -2.0), Complex32::new(-1.0, 1.0)];
     factor.solve_vector_in_place(&mut b)?;
-    assert_slice_close(&b, &[Complex32::new(1, 0), Complex32::new(2, 0)], 1e-4);
+    assert_slice_close(&b, &[Complex32::new(1.0, 0.0), Complex32::new(2.0, 0.0)], 1e-4);
     Ok(())
 }
