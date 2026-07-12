d3f4d1ba1e08c471a1d0d6375ad46f04dbd048ec
Evgenii Nekhoroshev <evgnekhoroshev@gmail.com>
2026-07-12 17:34:29 +1000

    Fix complex literals in Hermitian c32 HPTRF example

--- examples/lapack_hermitian_c32_hptrf.rs
+++ examples/lapack_hermitian_c32_hptrf.rs
@@ -8,7 +8,7 @@ use num_complex::Complex32;
 use matrixpacked::PackedHermitian;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let a = PackedHermitian::<Complex32>::from_vec(2, vec![Complex32::new(2, 0), Complex32::new(1, 1), Complex32::new(-1, 0)])?;
+    let a = PackedHermitian::<Complex32>::from_vec(2, vec![Complex32::new(2.0, 0.0), Complex32::new(1.0, 1.0), Complex32::new(-1.0, 0.0)])?;
     let factor = a.factorize_in_place()?;
     assert_eq!(factor.dimension(), 2);
     Ok(())
